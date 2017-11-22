460b4927dc17fb55d314b078e0bd0dc331cb7a1a
Liam Bostock <liam.bostock@microlise.com>
2017-11-22 11:46:48 +0000

    Merged PR 7763: putting response request objects behind an interface so that it supports unit
    putting response request objects behind an interface so that it supports unit testing
    
    Related work items: #26988

--- MicroService.Core.Api/Microlise.MicroService.Core.Api/BuiltInVerbHandlers/GetDescription.cs
+++ MicroService.Core.Api/Microlise.MicroService.Core.Api/BuiltInVerbHandlers/GetDescription.cs
@@ -9,10 +9,11 @@ namespace Microlise.MicroService.Core.Api.BuiltInVerbHandlers
     [ActivityHandler("GetApiDescription", HttpVerb.Get, "description", "Describes the API based on descriptions provided in the ActivityHandlerAttribute constructor for each VerbHandler class")]
     internal class GetDescription : IActivityHandler
     {
-        public void Handle(HttpRequest request, HttpResponse response)
+        public void Handle(IHttpRequest request, IHttpResponse response)
         {
             Dictionary<Type, ActivityHandlerAttribute[]> handlers = Container.FindAttributedTypes<ActivityHandlerAttribute>();
             List<object> handlerInfoList = new List<object>();
+            
             foreach (KeyValuePair<Type, ActivityHandlerAttribute[]> keyValuePair in handlers)
             {
                 foreach (ActivityHandlerAttribute attribute in keyValuePair.Value)
