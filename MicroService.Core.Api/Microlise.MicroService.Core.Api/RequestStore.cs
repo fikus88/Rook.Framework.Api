4919bbee08371fde13ec829058ef006536f128d0
Hugh Phoenix-Hulme <hugh.phoenix-hulme@microlise.com>
2017-11-23 10:55:10 +0000

    Added some logging

--- MicroService.Core.Api/Microlise.MicroService.Core.Api/RequestStore.cs
+++ MicroService.Core.Api/Microlise.MicroService.Core.Api/RequestStore.cs
@@ -16,133 +16,138 @@ using MongoDB.Driver;
 
 namespace Microlise.MicroService.Core.Api
 {
-	public sealed class RequestStore : IRequestStore
-	{
-		private const int MaxTimeToWaitForBusResponseInMilliseconds = 5000;
+    public sealed class RequestStore : IRequestStore
+    {
+        private const int MaxTimeToWaitForBusResponseInMilliseconds = 5000;
 
-		private readonly IQueueWrapper queueWrapper;
+        private readonly IQueueWrapper queueWrapper;
 
-		private readonly ILogger logger;
-		private readonly IMongoStore mongo;
-		private readonly IDateTimeProvider dateTimeProvider;
+        private readonly ILogger logger;
+        private readonly IMongoStore mongo;
+        private readonly IDateTimeProvider dateTimeProvider;
 
-		public RequestStore(
-			IDateTimeProvider dateTimeProvider,
-			IQueueWrapper queueWrapper,
-			ILogger logger,
-			IMongoStore mongo)
-		{
-			this.queueWrapper = queueWrapper;
-			this.logger = logger;
-			this.mongo = mongo;
-			this.dateTimeProvider = dateTimeProvider;
-		}
+        public RequestStore(
+            IDateTimeProvider dateTimeProvider,
+            IQueueWrapper queueWrapper,
+            ILogger logger,
+            IMongoStore mongo)
+        {
+            logger.Trace(nameof(RequestStore) + " constructor");
+            this.queueWrapper = queueWrapper;
+            this.logger = logger;
+            this.mongo = mongo;
+            this.dateTimeProvider = dateTimeProvider;
+        }
 
-		public static readonly ConcurrentDictionary<Guid, DataWaitHandle<object>> RequestsStore = new ConcurrentDictionary<Guid, DataWaitHandle<object>>();
+        public static readonly ConcurrentDictionary<Guid, DataWaitHandle<object>> RequestsStore = new ConcurrentDictionary<Guid, DataWaitHandle<object>>();
 
-		public Func<Guid> CreateUniqueId { get; set; } = Guid.NewGuid;
-	    public static List<string> Methods = new List<string>();
+        public Func<Guid> CreateUniqueId { get; set; } = Guid.NewGuid;
+        public static List<string> Methods = new List<string>();
 
-	    public void PublishAndWaitForResponse<TNeed,TSolution>(Message<TNeed,TSolution> message, HttpStatusCode successResponseCode, HttpResponse response)
-		{
+        public void PublishAndWaitForResponse<TNeed, TSolution>(Message<TNeed, TSolution> message, HttpStatusCode successResponseCode, HttpResponse response)
+        {
             Guid requestId = CreateUniqueId.Invoke();
 
-			message.Uuid = requestId;
-			message.LastModifiedBy = ServiceInfo.Name;
-			message.LastModifiedTime = dateTimeProvider.UtcNow;
+            message.Uuid = requestId;
+            message.LastModifiedBy = ServiceInfo.Name;
+            message.LastModifiedTime = dateTimeProvider.UtcNow;
 
             if (!Methods.Contains(message.Method))
                 Methods.Add(message.Method);
 
-			logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(PublishAndWaitForResponse)}\" Event=\"Preparing to publish message\" MessageId=\"{requestId}\" MessageMethod=\"{message.Method}\"");
+            logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(PublishAndWaitForResponse)}\" Event=\"Preparing to publish message\" MessageId=\"{requestId}\" MessageMethod=\"{message.Method}\"");
 
-			using (var dataWaitHandle = new DataWaitHandle<object>(false, EventResetMode.AutoReset))
-			{
-				if (RequestsStore.TryAdd(requestId, dataWaitHandle))
-				{
-					queueWrapper.PublishMessage(message);
+            using (var dataWaitHandle = new DataWaitHandle<object>(false, EventResetMode.AutoReset))
+            {
+                if (RequestsStore.TryAdd(requestId, dataWaitHandle))
+                {
+                    queueWrapper.PublishMessage(message);
 
-					DataWaitHandle<object> dataWaitHandleFromRemove;
+                    if (dataWaitHandle.WaitOne(MaxTimeToWaitForBusResponseInMilliseconds))
+                    {
+                        RequestsStore.TryRemove(requestId, out _);
 
-					if (dataWaitHandle.WaitOne(MaxTimeToWaitForBusResponseInMilliseconds))
-					{
-						RequestsStore.TryRemove(requestId, out dataWaitHandleFromRemove);
+                        var completedMessage = ((JObject)dataWaitHandle.Data).ToObject<Message<object, object>>();
 
-						var completedMessage = ((JObject)dataWaitHandle.Data).ToObject<Message<object, object>>();
+                        logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(PublishAndWaitForResponse)}\" Event=\"Published message and received response\" MessageId=\"{requestId}\" MessageMethod=\"{message.Method}\" Message=\"{completedMessage}\"");
 
-						logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(PublishAndWaitForResponse)}\" Event=\"Published message and received response\" MessageId=\"{requestId}\" MessageMethod=\"{message.Method}\" Message=\"{completedMessage}\"");
+                        if (completedMessage.Errors.Any())
+                        {
+                            response.SetObjectContent(completedMessage.Errors);
 
-						if (completedMessage.Errors.Any())
-						{
-							response.SetObjectContent(completedMessage.Errors);
+                            if (completedMessage.Errors.Any(e => e.Type == ResponseError.ErrorType.Server))
+                                response.HttpStatusCode = HttpStatusCode.InternalServerError;
+                            else
+                                response.HttpStatusCode = HttpStatusCode.BadRequest;
+                            return;
+                        }
 
-							if (completedMessage.Errors.Any(e => e.Type == ResponseError.ErrorType.Server))
-								response.HttpStatusCode = HttpStatusCode.InternalServerError;
-							else
-								response.HttpStatusCode = HttpStatusCode.BadRequest;
-							return;
-						}
+                        response.SetObjectContent(completedMessage.Solution);
+                        response.HttpStatusCode = successResponseCode;
+                        return;
+                    }
 
-						response.SetObjectContent(completedMessage.Solution);
-						response.HttpStatusCode = successResponseCode;
-						return;
-					}
+                    RequestsStore.TryRemove(requestId, out _);
 
-					RequestsStore.TryRemove(requestId, out dataWaitHandleFromRemove);
+                    response.SetStringContent("Failed to get a response from the bus");
+                    logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(PublishAndWaitForResponse)}\" Event=\"Published message and received no response\" MessageId=\"{requestId}\" MessageMethod=\"{message.Method}\" Message=\"{message}\"");
+                    response.HttpStatusCode = HttpStatusCode.RequestTimeout;
+                    return;
+                }
+            }
 
-					response.SetStringContent("Failed to get a response from the bus");
-					logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(PublishAndWaitForResponse)}\" Event=\"Published message and received no response\" MessageId=\"{requestId}\" MessageMethod=\"{message.Method}\" Message=\"{message}\"");
-					response.HttpStatusCode = HttpStatusCode.RequestTimeout;
-					return;
-				}
-			}
+            response.SetStringContent("Failed to publish request to the bus");
+            logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(PublishAndWaitForResponse)}\" Event=\"Failed to publish message\" MessageId=\"{requestId}\" MessageMethod=\"{message.Method}\" Message=\"{message}\"");
+            response.HttpStatusCode = HttpStatusCode.InternalServerError;
+        }
 
-			response.SetStringContent("Failed to publish request to the bus");
-			logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(PublishAndWaitForResponse)}\" Event=\"Failed to publish message\" MessageId=\"{requestId}\" MessageMethod=\"{message.Method}\" Message=\"{message}\"");
-			response.HttpStatusCode = HttpStatusCode.InternalServerError;
-		}
+        public bool FindResponse(MessageWrapper messageWrapper)
+        {
+            bool idFound = RequestsStore.TryGetValue(messageWrapper.Uuid, out DataWaitHandle<object> dataWaitHandle);
 
-		public bool FindResponse(MessageWrapper messageWrapper)
-		{
-			bool idFound = RequestsStore.TryGetValue(messageWrapper.Uuid, out DataWaitHandle<object> dataWaitHandle);
+            if (idFound)
+            {
+                logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(FindResponse)}\" Event=\"Found Id in requests store\" MessageId=\"{messageWrapper.Uuid}\" Data=\"{messageWrapper.Message}\"");
+                dataWaitHandle.Set(messageWrapper.Message);
+            }
+            else
+            {
+                logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(FindResponse)}\" Event=\"Could not find Id in requests store\" MessageId=\"{messageWrapper.Uuid}\" Data=\"{messageWrapper.Message}\"");
+            }
 
-			if (idFound)
-			{
-				logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(FindResponse)}\" Event=\"Found Id in requests store\" MessageId=\"{messageWrapper.Uuid}\" Data=\"{messageWrapper.Message}\"");
-				dataWaitHandle.Set(messageWrapper.Message);
-			}
-			else
-			{
-				logger.Trace($"Operation=\"{nameof(RequestsStore)}.{nameof(FindResponse)}\" Event=\"Could not find Id in requests store\" MessageId=\"{messageWrapper.Uuid}\" Data=\"{messageWrapper.Message}\"");
-			}
+            return idFound;
+        }
 
-			return idFound;
-		}
+        private Task waitLoopTask;
 
-		private Task waitLoopTask;
+        public void Start()
+        {
+            logger.Trace(nameof(RequestStore) + "." + nameof(Start));
+            waitLoopTask = new Task(WaitLoop);
+            waitLoopTask.Start();
+        }
 
-		public void Start()
-		{
-			waitLoopTask = new Task(WaitLoop);
-			waitLoopTask.Start();
-		}
+        private void WaitLoop()
+        {
+            logger.Trace(nameof(RequestStore) + "." + nameof(WaitLoop));
+            IMongoCollection<MessageWrapper> collection = mongo.GetCappedCollection<MessageWrapper>();
+            FindOptions<MessageWrapper> options = new FindOptions<MessageWrapper> { CursorType = CursorType.TailableAwait };
 
-		private void WaitLoop()
-		{
-			IMongoCollection<MessageWrapper> collection = mongo.GetCappedCollection<MessageWrapper>();
-			FindOptions<MessageWrapper> options = new FindOptions<MessageWrapper> { CursorType = CursorType.TailableAwait };
-
-			while (true)
-			{
-				using (IAsyncCursor<MessageWrapper> cursor = collection.FindSync(mw => mw.Message != null, options))
-				{
-					cursor.ForEachAsync(mw =>
-					{
-						if (FindResponse(mw))
-							mongo.RemoveEntity(mw);
-					});
-				}
-			}
-		}
-	}
+            while (true)
+            {
+                using (IAsyncCursor<MessageWrapper> cursor = collection.FindSync(mw => mw.Message != null, options))
+                {
+                    cursor.ForEachAsync(mw =>
+                    {
+                        logger.Trace(nameof(RequestStore) + "." + nameof(WaitLoop), new LogItem("Action","Item added to the capped collection"));
+                        if (FindResponse(mw))
+                        {
+                            logger.Trace(nameof(RequestStore) + "." + nameof(WaitLoop), new LogItem("Action", "Item removed from the capped collection"));
+                            mongo.RemoveEntity(mw);
+                        }
+                    });
+                }
+            }
+        }
+    }
 }
