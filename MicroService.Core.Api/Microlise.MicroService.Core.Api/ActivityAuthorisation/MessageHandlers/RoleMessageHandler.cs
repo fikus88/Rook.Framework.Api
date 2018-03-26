881a05a9aeebcee179d7b22508d8aa4a450e184f
Hugh Phoenix-Hulme <hugh.phoenix-hulme@microlise.com>
2018-03-26 17:13:51 +0100

    Updated RoleMessageHandler to use Backplane Sync across instances
    Added /health endpopint - returns 500 Server Error until there's some implementation

--- MicroService.Core.Api/Microlise.MicroService.Core.Api/ActivityAuthorisation/MessageHandlers/RoleMessageHandler.cs
+++ MicroService.Core.Api/Microlise.MicroService.Core.Api/ActivityAuthorisation/MessageHandlers/RoleMessageHandler.cs
@@ -1,21 +1,42 @@
+using System;
 using Microlise.MicroService.Core.Application.Message;
 using Microlise.MicroService.Core.Application.MessageHandlers;
 using Microlise.MicroService.Core.Attributes;
+using Microlise.MicroService.Core.Backplane;
 
-namespace Microlise.MicroService.Core.Api.ActivityAuthorisation.MessageHandlers {
+namespace Microlise.MicroService.Core.Api.ActivityAuthorisation.MessageHandlers
+{
     [Handler("RegisterActivityForAuthorisation", AcceptanceBehaviour = AcceptanceBehaviour.OnlyWithSolution)]
     [Handler("GetRolesForActivity", AcceptanceBehaviour = AcceptanceBehaviour.OnlyWithoutSolution)]
-    internal class RoleMessageHandler : IMessageHandler2<string, string> {
+    internal class RoleMessageHandler : IMessageHandler2<string, string>, IBackplaneConsumer
+    {
         private readonly IActivityAuthorisationManager activityAuthorisationManager;
-        public RoleMessageHandler(IActivityAuthorisationManager activityAuthorisationManager)
+        private readonly IBackplane backplane;
+
+        public RoleMessageHandler(IActivityAuthorisationManager activityAuthorisationManager, IBackplane backplane)
         {
             this.activityAuthorisationManager = activityAuthorisationManager;
+            this.backplane = backplane;
+        }
+
+        public Guid ConsumesType => typeof(ActivityRoles).GUID;
+
+        public void Consume(object v)
+        {
+            ActivityRoles ar = (ActivityRoles)v;
+            activityAuthorisationManager.ActivityRoles[ar.Activity] = ar.Roles;
         }
 
         public CompletionAction Handle(Message<string, string> message)
         {
-            activityAuthorisationManager.ActivityRoles[message.Need] = message.Solution;
+            backplane.Send(new ActivityRoles { Activity = message.Need, Roles = message.Solution });
             return CompletionAction.DoNothing;
         }
+    }
+
+    public class ActivityRoles
+    {
+        public string Activity;
+        public string[] Roles;
     }
 }
