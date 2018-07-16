ea49cea21956516c44071ef29eb24ccb177ad90b
Ian Dykes <ian.dykes@microlise.com>
2018-07-16 14:51:53 +0100

    Merged PR 14896: Add core_api_version to microlise_service_build_info metric
    Add core_api_version to microlise_service_build_info metric

---
+++ MicroService.Core.Api/Microlise.MicroService.Core.Api/ApiBuildInfoLabelProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microlise.MicroService.Core.Monitoring;
+using System.Collections.Generic;
+
+namespace Microlise.MicroService.Core.Api
+{
+    /// <summary>
+    /// Provides the build info labels for Microlise.MicroService.Core.Api
+    /// </summary>
+    public class ApiBuildInfoLabelProvider : IBuildInfoLabelProvider
+    {
+        public IEnumerable<BuildInfoLabel> GetBuildInfoLabels()
+        {
+            return new[]
+            {
+                new BuildInfoLabel("core_api_version", GetCoreApiVersion())
+            };
+        }
+        private static string GetCoreApiVersion()
+        {
+            return typeof(ApiBuildInfoLabelProvider).Assembly.GetName().Version.ToString();
+        }
+    }
+}
